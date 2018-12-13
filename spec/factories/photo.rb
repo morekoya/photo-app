e4636e58e0a3856975c662c9028ec4134525a475
Morolake Orekoya <morolake.orekoya@andela.com>
2018-12-13 14:14:06 +0100

    clean up specs

--- spec/factories/photo.rb
+++ spec/factories/photo.rb
@@ -2,7 +2,7 @@
 FactoryBot.define do
   factory :photo do
     name { FFaker::Lorem.word }    
-    user { user }
+    user_id { user.id }
     images { [ File.open(File.join(Rails.root, 'public', 'apple-touch-icon.png')) ]}
   end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,11 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  describe "Photo attibutes" do
-    it { should have_attached_file(:images) }
-    it { should validate_attachment_presence(:images) }
-    it { should validate_attachment_content_type(:photos).
-                allowing('image/png', 'image/gif').
-                rejecting('text/plain', 'text/xml') }
-  end
 end
