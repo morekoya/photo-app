da732b3324e9a51bcb4a4945253a818e1af14190
Morolake Orekoya <morolake.orekoya@andela.com>
2018-12-14 01:46:08 +0100

    upload s picture

--- db/migrate/20181106114218_create_welcomes.rb
+++ db/migrate/20181106114218_create_welcomes.rb
@@ -2,7 +2,6 @@ class CreateWelcomes < ActiveRecord::Migration[5.1]
   def change
     create_table :welcomes do |t|
       t.string :index
-
       t.timestamps
     end
   end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,6 @@
 
 FactoryBot.define do
+  
   factory :user do
     email { FFaker::Internet.email }
     password { "p4ssw0rd" }
