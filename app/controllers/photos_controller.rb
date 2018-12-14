a9570bcc2685e36f8a45cb39248d39674b15416f
Morolake Orekoya <morolake.orekoya@andela.com>
2018-12-14 02:01:34 +0100

    allow users to delete specified picture

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,6 +3,37 @@ class PhotosController < ApplicationController
     @photos = current_user.photos(&:images)
    end
   
+   def new
+    @photo = Photo.new
+   end
+  
+   def create
+    photos = Photo.new
+    
+    photos.images << photos.images.attach(params[:photo][:image])
+    if current_user.photos << photos
+      photos.images.first.name = 'woah'
+      flash[:notice] = "Successfully added new photo!"
+      redirect_to photos_path
+    else
+     flash[:alert] = "Error adding new photo!"
+     render :new
+    end
+   end
+
+  def show
+    @photo = current_user.photos(&:images).find(params[:id])
+  end
+
+  def destroy
+    @photo = Photo.find( params[:id])
+      if @photo.images.first.purge
+        flash[:notice] = "Successfully deleted photo!"
+        redirect_to root_path
+      else
+        flash[:alert] = "Error deleting photo!"
+      end
+    end
   private
 
   def photo_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ root 'photos#index'
 devise_for :users
 resources :photos
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
