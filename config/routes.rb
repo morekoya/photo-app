9044c2ac160278a8ff6fc74251c13c0dadc6f923
Morolake Orekoya <morolake.orekoya@andela.com>
2018-12-19 11:35:36 +0100

    make unrestful entry restful

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ root 'photos#index'
 devise_for :users
 resources :photos
 resources :users
-get 'user-pictures' => 'photos#user_pictures'
+get 'user-pictures', to: 'photos#user_pictures'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
